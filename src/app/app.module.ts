673398c39e5b927dc054e2bf5d5abb113acd1488
Doan Quoc Cuong <cuong.dq@mobifone.vn>
2018-10-10 15:52:51 +0700

    Form home ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { TooltipsModule } from 'ionic-tooltips';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -14,7 +18,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    TooltipsModule,
+    BrowserAnimationsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,105 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  isShowHeader: boolean = false;
+  isShowSearch: boolean = false;
+  isShowRoute: boolean = false;
+  isShowCenter: boolean = false;
+  isShowFooter: boolean = false;
 
-  constructor(public navCtrl: NavController) {
+  searchOrigin: string = "";
+  searchDestination: string = "";
+
+  className: string = "icon-center icon-blue";
+
+  constructor(public navCtrl: NavController,
+    private toastCtrl: ToastController) { }
+
+  ngOnInit() {
+    /* this.isShowSearch = false;
+    this.isShowRoute = false;
+    this.isShowCenter = false;
+    this.isShowFooter = false; */
+  }
+
+  showCenterMode(f: number) {
+    //this.isShowFooter = true;
+    if (f == 1) {
+      //tim theo diem dau
+      this.toastCtrl.create({
+        message: "Di chuyển bản đồ tới vị trí điểm bắt đầu",
+        duration: 3000,
+        position: 'bottom'
+      }).present();
+      //class mau xanh
+      this.className = "icon-center icon-blue";
+      this.isShowCenter = true;
+
+    }
+    if (f == 2) {
+      //tim theo diem cuoi
+      this.toastCtrl.create({
+        message: "Di chuyển bản đồ tới vị trí điểm đến",
+        duration: 3000,
+        position: 'bottom'
+      }).present();
+      //class mau do
+      this.className = "icon-center icon-red";
+      this.isShowCenter = true;
+    }
+  }
+
+
+  showSearchMode(f: number) {
+    if (f == 1) {
+      //hien thi o tim kiem diem den
+      this.toastCtrl.create({
+        message: "Nhập địa chỉ điểm đến hoặc tìm trên bản đồ",
+        duration: 1000,
+        position: 'middle'
+      }).present();
+      this.isShowRoute = true;
+      this.isShowCenter = false;
+    }
+    if (f == 2) {
+      //tim theo diem cuoi
+      this.toastCtrl.create({
+        message: "Đang tìm kiếm đường đi",
+        duration: 1000,
+        position: 'middle'
+      }).present();
+      this.isShowCenter = true;
+    }
+  }
+
+  searchEnter(f: number) {
 
   }
+
+  showSearchbar() {
+    this.isShowHeader = !this.isShowHeader;
+    this.isShowSearch = !this.isShowSearch;
+  }
+
+  showRoutebar() {
+    this.isShowRoute = !this.isShowRoute;
+  }
+
+  showCurrentLocation() {
+
+  }
+
+  resetMap() {
+
+  }
+
+
+
+
 
 }
